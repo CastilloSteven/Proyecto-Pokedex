22b47ef8abe6d7ba113084bb73eda1562e19bc9e
Steven <sacastillo889@misena.edu.co>
2022-11-12 20:36:30 -0500

    paginacion

--- logica.js
+++ logica.js
@@ -1,38 +1,49 @@
-function getPokemon (id){
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}/`
-    fetch(url)
+const anterior = document.getElementById('anterior')
+const siguiente = document.getElementById('siguiente')
+const targetas = document.getElementById('targetas')
+let primero = 1;
+let ultimo = 7;
+
+
+
+async function getPokemon (id){
+    const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    /*let url = `https://pokeapi.co/api/v2/pokemon/${id}/`
+    fetch(url)*/
         .then(Response => Response.json())
         .then(data => pintar(data))
         .catch(error => console.log(error))
 }
 
-/*function pintar(data){
-    let contenido = `<tr><td>${data.id}</td><td>${data.name}</td><td><img src="${data.sprites.front_default}" alt="${data.id}"</td></tr>`;
-    document.getElementById('tabla').innerHTML += contenido;
-}*/
-
 function pintar (data){
-    let contenido = `<article id="pokemones"><img src="${data.sprites.front_default}" alt="w" id="imagenes"><span id="nombrePokemon">${data.name}</span> <br><span id="idPokemon">${data.id}</span></article>`
+    let contenido = `<article id="pokemones"><img src="${data.sprites.front_default}" alt="${data.name}" id="imagenes"><span id="nombrePokemon">${data.name}</span> <br><span id="idPokemon">${data.id}</span></article>`
     document.getElementById('targetas').innerHTML += contenido;
 }
 
-function randomId (){
-    for(let i = 0; i < 2; i++){
-        let idRandom = (Math.floor(Math.random()*150));
-        console.log(idRandom)
-        getPokemon(idRandom)
+function orden (primero, ultimo){
+    for (let i = primero; i <= ultimo + primero; i++){
+        getPokemon(i)
     }
 }
 
-nuevo.onclick = function() {
-    randomId()
+anterior.addEventListener('click', () => {
+    if(primero != 1){
+        primero -= 8;
+        limpiar(targetas);
+        orden (primero, ultimo);
+    }
+})
+
+siguiente.addEventListener('click', () => {
+        primero += 8;
+        limpiar(targetas);
+        orden (primero, ultimo);
+})
+
+function limpiar (parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild)
+    }
 }
 
-/*
-
-let nuevo = document.getElementById('nuevo')
-
-nuevo.onclick = function() {
-    let contenido = '<article id="pokemones"><img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/194.png" alt="w" id="imagenes"><span id="nombrePokemon">asdqwe</span> <br><span id="idPokemon">1</span></article>'
-    document.getElementById('targetas').innerHTML += contenido;
-}*/
+orden(primero, ultimo);
