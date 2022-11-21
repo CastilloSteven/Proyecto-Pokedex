8120f70d0f5fa6fc85a82dc7c19397fe38dbaa6c
Steven <sacastillo889@misena.edu.co>
2022-11-21 13:49:28 -0500

    correccion de escritura

--- logica.js
+++ logica.js
@@ -1,14 +1,12 @@
 const anterior = document.getElementById('anterior')
 const siguiente = document.getElementById('siguiente')
-const targetas = document.getElementById('targetas')
+const tarjetas = document.getElementById('tarjetas')
 const volver = document.getElementById('volver')
 const navLista = document.getElementById('navLista')
 let buscador = document.getElementById('buscador')
 let buscar = document.getElementById('buscar')
 let primero = 1;
 let ultimo = 8;
-
-
 
 function getPokemon (id){
     const url = fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
@@ -18,8 +16,8 @@ function getPokemon (id){
 }
 
 function pintar (data){
-    let contenido = `<article id="pokemones"><img src="${data.sprites.front_default}" alt="${data.name}" id="imagenes"><span id="nombrePokemon">${data.name}</span> <br><span id="idPokemon">${data.id}</span></article>`
-    document.getElementById('targetas').innerHTML += contenido;
+    let contenido = `<article id="pokemones"><img src="${data.sprites.front_default}" alt="${data.name}" id="imagenes"><span id="nombrePokemon">${data.name}</span></article>`
+    document.getElementById('tarjetas').innerHTML += contenido;
 }
 
 function orden (primero, ultimo){
@@ -30,26 +28,26 @@ function orden (primero, ultimo){
 
 buscar.onclick = function () {
     let id = buscador.value
-    limpiar(targetas)
+    limpiar(tarjetas)
     getPokemon(id)
 }
 
 volver.addEventListener('click', () => {
-    limpiar(targetas);
+    limpiar(tarjetas);
     orden (primero, ultimo);
 })
 
 anterior.addEventListener('click', () => {
     if(primero != 1){
         primero -= 9;
-        limpiar(targetas);
+        limpiar(tarjetas);
         orden (primero, ultimo);
     }
 })
 
 siguiente.addEventListener('click', () => {
         primero += 9;
-        limpiar(targetas);
+        limpiar(tarjetas);
         orden (primero, ultimo);
 })
 
